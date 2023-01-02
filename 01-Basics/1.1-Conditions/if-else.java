f9fac00237adf647986fbe04bb1e3ac0dcb701a7
Arun Khajuria <83227294+thearunkhajuria@users.noreply.github.com>
2023-01-02 13:56:52 +0530

    Create if-else.java

---
+++ 01-Basics/1.1-Conditions/if-else.java
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/java-if-else-decision-making0924/0?category%5B%5D=Java&category%5B%5D=Java&difficulty%5B%5D=-2&page=1&query=category%5B%5DJavadifficulty%5B%5D-2page1category%5B%5DJava
+
+// Problem : if-else (Decision Making)
+
+
+//User function Template for Java
+class Solution{
+    static String compareNM(int n,int m){
+        // code here
+        if(n<m){
+            return "lesser";
+        }
+        else if(n==m){
+            return "equal";
+        }
+        else 
+            return "greater";
+        
+    }
+    //main end
+}
