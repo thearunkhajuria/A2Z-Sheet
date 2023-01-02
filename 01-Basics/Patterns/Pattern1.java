2d92004196761bd578539ff85de0e9f1bd7e92a1
Arun Khajuria <83227294+thearunkhajuria@users.noreply.github.com>
2023-01-02 14:01:53 +0530

    Update Pattern1.java

--- 01-Basics/Patterns/Pattern1.java
+++ 01-Basics/Patterns/Pattern1.java
@@ -1,4 +1,11 @@
 //https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
+/* Pattern 1
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
 class Solution {
 
     void printSquare(int n) {
